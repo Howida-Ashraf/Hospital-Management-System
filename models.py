04ce91189b006f77c78850cebe0dea4d11e5d224
Mariam.M <mariammohammeds0712@gmail.com>
2026-08-27 11:33:46 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, person_id, name, age, contact_info):
+        self.id = person_id
+        self.name = name
+        self.age = age
+        self.contact_info = contact_info
+
+    def display_info(self):
+        print(f"ID: {self.id}, Name: {self.name}, Age: {self.age}, Contact: {self.contact_info}")
+
+
+class Patient(Person):
+    def __init__(self, patient_id, name, age, contact_info, medical_history=""):
+        super().__init__(patient_id, name, age, contact_info)
+        self.medical_history = medical_history
+
+    def display_info(self):
+        print("Patient Information ")
+        super().display_info()
+        print(f"Medical History: {self.medical_history}")
+
+    def update_info(self, name=None, contact_info=None):
+        if name:
+            self.name = name
+        if contact_info:
+            self.contact_info = contact_info
