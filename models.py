d599dc8343a08b1dfcaa3d679964b9a5302376ae
Mariam.M <mariammohammeds0712@gmail.com>
2026-08-27 12:13:29 +0200

    Implement patient registration and search functionality
    Added validation for patient name during initialization and included functions for registering and searching patients.

--- models.py
+++ models.py
@@ -11,11 +11,14 @@ class Person:
 
 class Patient(Person):
     def __init__(self, patient_id, name, age, contact_info, medical_history=""):
+        if name == "":
+            print("Invalid name, patient not created properly")
+
         super().__init__(patient_id, name, age, contact_info)
         self.medical_history = medical_history
 
     def display_info(self):
-        print("Patient Information ")
+        print("Patient Information")
         super().display_info()
         print(f"Medical History: {self.medical_history}")
 
@@ -24,3 +27,16 @@ class Patient(Person):
             self.name = name
         if contact_info:
             self.contact_info = contact_info
+
+
+def register_patient(patients_list, patient):
+    patients_list.append(patient)
+    print(f"Patient {patient.name} registered successfully")
+
+
+def search_patient(patients_list, patient_id):
+    for p in patients_list:
+        if p.id == patient_id:
+            return p
+    print("Patient not found")
+    return None
